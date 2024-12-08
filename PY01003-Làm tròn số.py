b4cd8bdd447314347f7c13a439a6bcc1f096a3cc
Khangvu345 <khangv6002@gmail.com>
2024-12-08 18:09:56 +0700

    UPDATE
    làm tròn từng mức của 1 số

--- PY01003-Làm tròn số.py
+++ PY01003-Làm tròn số.py
@@ -1,2 +1,10 @@
-a=10000000
-print(len(str(a)))
+def lam_tron_so(N):
+    factor = 10
+    while N >= factor:
+        N = N + (factor - N % factor) if N % factor >= factor // 2 else N - N % factor
+        factor *= 10
+    return N
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        print(lam_tron_so(int(input())))
