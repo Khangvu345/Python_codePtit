95d65179e367db0a79e4f1d4dc6b0796b47ee3f7
Khangvu345 <khangv6002@gmail.com>
2024-12-12 03:13:50 +0700

    UPDATE

--- PY01003-Làm tròn số.py
+++ PY01003-Làm tròn số.py
@@ -1,10 +1,10 @@
 def lam_tron_so(N):
     factor = 10
     while N >= factor:
-        N = N + (factor - N % factor) if N % factor >= factor // 2 else N - N % factor
+        N = N - (N % factor) + factor if N % factor >= factor // 2 else N - (N % factor)
         factor *= 10
     return N
-
 if __name__ == "__main__":
     for _ in range(int(input())):
-        print(lam_tron_so(int(input())))
+        N = int(input())
+        print(lam_tron_so(N))
